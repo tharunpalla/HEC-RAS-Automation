5c53b014aac8257fe39f95e885f96c40d10ad19b
kunalpatange <kunalpatange1111@gmail.com>
2022-02-24 23:10:28 -0500

    added one page post submission

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -7,6 +8,9 @@ app = Flask(__name__)
 def hello_world():  # put application's code here
     return render_template('index.html')
 
+@app.route('/getData',methods=['POST'])
+def getData():
+    return "Loading the Data ..."
 
 if __name__ == '__main__':
     app.run(debug=True)
